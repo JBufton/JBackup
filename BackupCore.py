db05d0e20b47a7de9473ded98d13566abaf9b920
JBufton <joshua.d.bufton@gmail.com>
2021-03-21 22:18:07 +0000

    added a bunch of main functions

--- BackupCore.py
+++ BackupCore.py
@@ -32,17 +32,20 @@ class JBackup_Core:
                     # Check if the current update file is in the current backup state
                     if updateFile in self.m_currentBackupState["files"].keys():
                         self.m_currentBackupState["files"][updateFile]["mtime"] = updateDict["files"][updateFile]["mtime"]
-                        self.m_currentBackupState["files"][updatefile]["changes"].append( updateDict["files"][updateFile]["change"] )
+                        self.m_currentBackupState["files"][updatefile]["changes"].append( updateDict["files"][updateFile]["changes"] )
                     else:
                         self.m_currentBackupState["files"][updateFile] = {}
                         self.m_currentBackupState["files"][updateFile]["mtime"] = updateDict["files"][updateFile]["mtime"]
-                        self.m_currentBackupState["files"][updatefile]["changes"] = [ updateDict["files"][updateFile]["change"] ]
+                        self.m_currentBackupState["files"][updatefile]["changes"] = [ updateDict["files"][updateFile]["changes"] ]
             else:
                 # We don't want to apply this update because it is after the date we want to sync to
                 return self.m_currentBackupState
 
     # Function to back if a file is updated vs the latest file
     def isFileUpdated(self, _path ):
+        # First check if the current backup state is the latest
+        if not self.m_currentBackupState["latest"]:
+            self.GetBackupState()
         if _path in self.m_currentBackupState["files"].keys():
             # The file already exists in the backup
             if getmtime( _path ) > self.m_currentBackupState["files"][_path]["mtime"]:
@@ -53,15 +56,73 @@ class JBackup_Core:
             # The file is new and so is updated by default
             return True
 
+    # Function that given a file path gets the contents of the latest backed up version of that file as a list of lines
+    def GetCurrentVersionOfFile(self, _path):
+        if _path in self.m_currentBackupState["files"].keys():
+            fileContents = ""
+            # Cycle through changes saved
+            for change in self.m_currentBackupState["files"][_path]["changes"]:
+                # Change is replacing the whole file, normally only made if the file doesn't exist previously
+                if change["type"] == "wholeFile":
+                    fileContents = change["content"]
+                # Change is deleting a line at a specific index
+                elif change["type"] == "deleteLine":
+                    splitFileContents = fileContents.split("\n")
+                    del splitFileContents[ change['lineNumber'] ]
+                    fileContents = splitFileContents.join("\n")
+                # Change is adding in a line at a specific index
+                elif change["type"] == "addLine":
+                    splitFileContents = fileContents.split("\n")
+                    splitFileContents.insert( change["lineNumber"], change["content"] )
+                    fileContents = splitFileContents.join("\n")
+            return fileContents.splitlines("\n")
+        else:
+            return None
+
+
+    # Function to find the differences between two files and return a list detailing those changes
+    def GetFileChanges( self, _newFile ):
+        newfileObject = open( _newFile, "r" )
+        newFileContents = newfileObject.readlines()
+        oldFileContents = self.GetCurrentVersionOfFile( _newFile )
+        changes = {}
+        # First check if there is no old file. Add the whole file if it didn't exist previously
+        if oldFileContents == None:
+            changes = {
+                "type": "wholeFile",
+                "content": _newFile
+            }
+            return changes
+        
+        # Cycle through the old file and compare it to the new one
+        for line in oldFileContents:
+            continue
+        return changes
+
+
+    # Function that returns a dictionary for the changes to a file vs the files currently backed up state
+    def BackupFile( self, _path ):
+        fileUpdate = {
+            "mtime": getmtime( _path ),
+            "changes": self.GetFileChanges( _path )
+        }
+
+        return fileUpdate
+
     # Function to recursively find all of the files changed in m_paths
     def UpdateBackup(self):
+        updateChanges = {
+            "ctime": datetime.now(),
+            "files": {}
+        }
+
         for path in self.m_paths:
             if isdir(path):
                 # Walk the directory tree
                 for root, dirs, files in os.walk( path ):
                     for filePath in [join(root, f) for f in files]:
                         if self.isFileUpdated( filePath ):
-                            # TODO self.BackupFile( filePath )
+                            updateChanges["files"][filePath] = self.BackupFile( _path )
             else:
                 if self.isFileUpdated( path ):
-                    # TODO self.BackupFile( path )
+                    updateChanges["files"][path] = self.BackupFile( path )
