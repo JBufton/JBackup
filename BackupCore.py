3e041ac6c86c47a565ae90330bbff33b991528a3
JBufton <joshua.d.bufton@gmail.com>
2021-03-22 09:32:20 +0000

    Added recover function

--- BackupCore.py
+++ BackupCore.py
@@ -1,20 +1,18 @@
-from os.path import isdir, getmtime, join
-from os import walk, listdir
+from os.path import isdir, getmtime, join, exists, dirname
+from os import walk, listdir, makedirs
 from datetime import datetime
 import pickle
 
 class JBackup_Core:
 
     def __init__(self):
-        # List of paths of all of the files and folders wanted for backup
-        self.m_paths = []
         # Location of where to store backup
         self.m_backupPath = ""
         # The list of files in the backup and their current states aswell as other information about the current backup
         self.m_currentBackupState = {}
 
     # Function to set m_currentBackupState to a specific date or latest if _date set to default
-    def GetBackupState(self, _date=None):
+    def GetBackupState( self, _date=None ):
         if _date == None:
             self.m_currentBackupState["latest"] = True
             _date = datetime.now()
@@ -28,21 +26,22 @@ class JBackup_Core:
             # Check to see if we need to add these changes
             if updateDict["ctime"] <= _date:
                 self.m_currentBackupState["ctime"] = updateDict["ctime"]
+                self.m_currentBackupState["backupPaths"] += updateDict["backupPaths"]
                 for updateFile in updateDict["files"].keys():
                     # Check if the current update file is in the current backup state
                     if updateFile in self.m_currentBackupState["files"].keys():
                         self.m_currentBackupState["files"][updateFile]["mtime"] = updateDict["files"][updateFile]["mtime"]
-                        self.m_currentBackupState["files"][updatefile]["changes"].append( updateDict["files"][updateFile]["changes"] )
+                        self.m_currentBackupState["files"][updateFile]["changes"].append( updateDict["files"][updateFile]["changes"] )
                     else:
                         self.m_currentBackupState["files"][updateFile] = {}
                         self.m_currentBackupState["files"][updateFile]["mtime"] = updateDict["files"][updateFile]["mtime"]
-                        self.m_currentBackupState["files"][updatefile]["changes"] = [ updateDict["files"][updateFile]["changes"] ]
+                        self.m_currentBackupState["files"][updateFile]["changes"] = [ updateDict["files"][updateFile]["changes"] ]
             else:
                 # We don't want to apply this update because it is after the date we want to sync to
                 return self.m_currentBackupState
 
     # Function to back if a file is updated vs the latest file
-    def isFileUpdated(self, _path ):
+    def isFileUpdated( self, _path ):
         # First check if the current backup state is the latest
         if not self.m_currentBackupState["latest"]:
             self.GetBackupState()
@@ -57,7 +56,7 @@ class JBackup_Core:
             return True
 
     # Function that given a file path gets the contents of the latest backed up version of that file as a list of lines
-    def GetCurrentVersionOfFile(self, _path):
+    def ReBuildFileFromBackup( self, _path):
         if _path in self.m_currentBackupState["files"].keys():
             fileContents = ""
             # Cycle through changes saved
@@ -75,27 +74,41 @@ class JBackup_Core:
                     splitFileContents = fileContents.split("\n")
                     splitFileContents.insert( change["lineNumber"], change["content"] )
                     fileContents = splitFileContents.join("\n")
-            return fileContents.splitlines("\n")
+            return fileContents
         else:
             return None
 
 
+    # Function to generate all of the files that are backed up to the desired location
+    def RebuildFiles( self, _outputDir ):
+        if not exists( _outputDir ):
+            makedirs( _outputDir )
+        for rebuildFile in self.m_currentBackupState["files"].keys():
+            pathToOutputFile = join( _outputDir, rebuildFile )
+            if not exists( dirname( pathToOutputFile ) ):
+                makedirs( dirname( pathToOutputFile ) )
+            fileContents = self.ReBuildFileFromBackup( rebuildFile )
+            with open( pathToOutputFile, "w" ) as outputRebuildFile:
+                outputRebuildFile.write( fileContents )
+
+
+
     # Function to find the differences between two files and return a list detailing those changes
-    def GetFileChanges( self, _newFile ):
+    def GetFileChanges( self, _oldFile, _newFile ):
+        # First we need to read the content of the file
         newfileObject = open( _newFile, "r" )
         newFileContents = newfileObject.readlines()
-        oldFileContents = self.GetCurrentVersionOfFile( _newFile )
-        changes = {}
+        changes = []
         # First check if there is no old file. Add the whole file if it didn't exist previously
-        if oldFileContents == None:
-            changes = {
+        if _oldFile == None:
+            changes.append({
                 "type": "wholeFile",
                 "content": _newFile
-            }
+            })
             return changes
         
         # Cycle through the old file and compare it to the new one
-        for line in oldFileContents:
+        for line in _oldFile.splitlines("\n"):
             continue
         return changes
 
@@ -104,25 +117,36 @@ class JBackup_Core:
     def BackupFile( self, _path ):
         fileUpdate = {
             "mtime": getmtime( _path ),
-            "changes": self.GetFileChanges( _path )
+            "changes": self.GetFileChanges( self.ReBuildFileFromBackup( _path ), _path )
         }
 
         return fileUpdate
 
     # Function to recursively find all of the files changed in m_paths
-    def UpdateBackup(self):
+    def UpdateBackup( self, _newPaths=[] ):
+        # This will only work on the latest files so we need to update the backup state to latest
+        self.GetBackupState()
         updateChanges = {
             "ctime": datetime.now(),
-            "files": {}
+            "files": {},
+            "backupPaths": _newPaths
         }
 
-        for path in self.m_paths:
+        backupPaths = self.m_currentBackupState['backupPaths'] + updateChanges["backupPaths"]
+
+        for path in backupPaths:
             if isdir(path):
                 # Walk the directory tree
-                for root, dirs, files in os.walk( path ):
+                for root, dirs, files in walk( path ):
                     for filePath in [join(root, f) for f in files]:
                         if self.isFileUpdated( filePath ):
-                            updateChanges["files"][filePath] = self.BackupFile( _path )
+                            updateChanges["files"][filePath] = self.BackupFile( path )
             else:
                 if self.isFileUpdated( path ):
                     updateChanges["files"][path] = self.BackupFile( path )
+        
+        pickleFileName = str({len( [i for i in listdir( self.m_backupPath ) if ".pkl" in i] ) + 1})
+        # Write out the new update
+        pickle.dump( updateChanges, open( join( self.m_backupPath, pickleFileName ), "wb" ) )
+        # Now we have written the update lets get the latest backup state to load those changes into the current backup state
+        self.GetBackupState()
